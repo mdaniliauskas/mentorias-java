e9fd48178ff63105ff1327b599de304b47abaa37
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-01-28 14:49:06 -0300

    Iniciando a classe MenuCalculadora que receberá todas calculadoras

---
+++ Mentoria1_Calculadoras/src/br/com/mentoria1/calculadoras/MenuCalculadora.java
@@ -0,0 +1,40 @@
+package br.com.mentoria1.calculadoras;
+
+/*
+Mentoria Java - Programa 01 - Desafio
+
+Calculadora
+
+Neste primeiro desafio, será testado e cobrado sua orientação a objeto, para isso deverá estudar os 4 pilares da orientação (Abstração, Encapsulamento, Polimorfismo e Herança).
+
+Onde teremos uma Classe principal que deverá ser usada somente como "menu" e será a origem para a chamada de objetos e métodos de outras classes, obedecendo menu a seguir:
+
+Calculadora Padrão
+Soma
+Subtração
+Divisão
+Multiplicação
+Potenciação
+
+Calculadora IMC
+
+Calculadora Área
+Circulo
+Quadrado
+Retângulo
+
+Dado: O inicio do programa Calculadora
+Quando: Escolher a opcao Calculadora Padrao
+E: Escolher a opcao soma
+E: Pedir a inserção de 2 valores
+Entao: devera realizar a soma de 2 entradas
+Com esse menu, ao iniciar o programa, deverá ser perguntado ao usuário qual tipo de função da calculadora ele deseja utilizar.
+
+Observação: Para este programa, Quero uma Cobertura de 85% de testes unitários
+ */
+
+public class MenuCalculadora {
+    public static void main(String[] args) {
+
+    }
+}
